5c7a106cf345d16389c55d01aeb362d60619eb95
Andrei Arkhipov <it.arkhipov@gmail.com>
2024-03-18 10:18:26 +0300

    refactor to one function

--- csv_transition.py
+++ csv_transition.py
@@ -8,109 +8,60 @@ list_limit = 3
 skipped_key = ['goods', 'states_history']
 
 
-def init_df(data, _df, prefix=''):
+def insert_df_value(data, _df, prefix=''):
     for key, value in data.items():
         if key in skipped_key:
             continue
         new_key = f"{prefix}{key}" if prefix else key
         if isinstance(value, dict):
-            init_df(value, _df, prefix=new_key + '_')
+            insert_df_value(value, _df, prefix=new_key + '_')
         elif 'custom_fields' in new_key:
             for custom_field in value[:custom_fields_limit]:
-                _df[f"{new_key}_{custom_field.get('id')}"] = [custom_field.get('value')]
+                _df.loc[_df.index[-1], f"{new_key}_{custom_field.get('id')}"] = custom_field.get('value')
         elif 'banned_tags' in new_key:
             for tag in value[:list_limit]:
-                _df[f"{new_key}_{tag}"] = 1
+                _df.loc[_df.index[-1], f"{new_key}_{tag}"] = 1
         elif 'required_vehicle_tags' in new_key:
             for tag in value[:list_limit]:
-                _df[f"{new_key}_{tag}"] = 1
+                _df.loc[_df.index[-1], f"{new_key}_{tag}"] = 1
         elif 'tags' in new_key:
             for tag in value[:list_limit]:
-                _df[f"{new_key}_{tag}"] = 1
+                _df.loc[_df.index[-1], f"{new_key}_{tag}"] = 1
         elif 'photos' in new_key or 'files' in new_key:
-            _df[new_key] = len(value)
+            _df.loc[_df.index[-1], new_key] = len(value)
         elif 'parks' in new_key and 'lated_by_parks' not in new_key:
             if value:
-                _df[new_key] = value[-1].get('end') - value[-1].get('begin')
+                _df.loc[_df.index[-1], new_key] = value[-1].get('end') - value[-1].get('begin')
             else:
-                _df[new_key] = 0
+                _df.loc[_df.index[-1], new_key] = 0
         elif 'zones' in new_key:
             if value:
-                _df[new_key] = value[0]
+                _df.loc[_df.index[-1], new_key] = value[0]
             else:
-                _df[new_key] = ''
-        # elif isinstance(value, list):
-        #     for i, element in enumerate(value[:list_limit]):
-        #         if isinstance(element, (dict, list, set, tuple)):
-        #             init_df(element, _df, prefix=f"{new_key}_{i}_")
-        #         else:
-        #             _df[f"{new_key}"] = element
-        #     else:
-        #         _df[f"{new_key}"] = ''
+                _df.loc[_df.index[-1], new_key] = ''
         else:
-            _df[new_key] = [value]
-    return _df
-
-
-def add_to_df(_index, data, _df, prefix=''):
-    for key, value in data.items():
-        if key in skipped_key:
-            continue
-        new_key = f"{prefix}{key}" if prefix else key
-        if isinstance(value, dict):
-            add_to_df(_index, value, _df, prefix=new_key + '_')
-        elif 'custom_fields' in new_key:
-            for custom_field in value[:custom_fields_limit]:
-                _df.loc[_index, f"{new_key}_{custom_field.get('id')}"] = custom_field.get('value')
-        elif 'banned_tags' in new_key:
-            for tag in value[:list_limit]:
-                _df.loc[_index, f"{new_key}_{tag}"] = 1
-        elif 'required_vehicle_tags' in new_key:
-            for tag in value[:list_limit]:
-                _df.loc[_index, f"{new_key}_{tag}"] = 1
-        elif 'tags' in new_key:
-            for tag in value[:list_limit]:
-                _df.loc[_index, f"{new_key}_{tag}"] = 1
-        elif 'photos' in new_key or 'files' in new_key:
-            _df.loc[_index, new_key] = len(value)
-        elif 'parks' in new_key and 'lated_by_parks' not in new_key:
-            if value:
-                _df.loc[_index, new_key] = value[-1].get('end') - value[-1].get('begin')
-            else:
-                _df.loc[_index, new_key] = 0
-        elif 'zones' in new_key:
-            if value:
-                _df.loc[_index, new_key] = value[0]
-            else:
-                _df.loc[_index, new_key] = ''
-        # elif isinstance(value, list):
-        #     for i, element in enumerate(value[:list_limit]):
-        #         if isinstance(element, (dict, list, set, tuple)):
-        #             add_to_df(_index, element, _df, prefix=f"{new_key}_{i}_")
-        #         else:
-        #             _df.loc[_index, f"{new_key}"] = element
-        #     else:
-        #         _df.loc[_index, f"{new_key}"] = ''
-        else:
-            _df.loc[_index, new_key] = value
+            _df.loc[_df.index[-1], new_key] = value
     return _df
 
 
 with open('orders_3.json', 'r', encoding='utf-8') as file:
-    # Load JSON data from the file
     orders = json.load(file)
 
 simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
 
 df = pd.DataFrame()
 
-init_df(orders[0], df)
-if len(orders) > 1:
-    for index, order in enumerate(orders[1:]):
-        add_to_df(index + 1, order, df)
+for order in orders:
+    new_row = pd.DataFrame({}, index=[0])
+    df = pd.concat([df, new_row], ignore_index=True)
+    insert_df_value(order, df)
 
-# for column in df.columns:
-#     if 'custom_fields' in column:
-#         df[column] = df[column].fillna(0)
-# df = df.fillna('')
+int_columns = ['date', 'departure_parks', 'arrival_parks', 'start_time', 'end_time', 'plan_time', 'load_time',
+               'created', 'version', 'modified', 'duration', 'photos', 'files', 'tags']
+for column in df.columns:
+    if 'tags' in column:
+        df[column] = df[column].fillna(0)
+    if any(string in column for string in int_columns):
+        df[column] = df[column].astype('int64')
+df = df.infer_objects(copy=False).fillna('')
 df.to_csv('sample_dataframe.csv', sep=';')
